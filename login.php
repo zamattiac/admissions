264654ebb0bd177c7cadac49721e2c9298748ce1
zamattiac <zamattiac@gmail.com>
2016-03-31 14:58:53 -0400

    eliminated password-based name setting

--- login.php
+++ login.php
@@ -43,8 +43,9 @@
         $name_in = strtolower($_POST["name_in"]);
         $pass_in = $_POST["pass_in"];
 
-        //$query = "SELECT * FROM graders WHERE Comp = " . $name_in . ";";
-        $query = "SELECT * FROM graders;";
+        $query = "SELECT * FROM graders WHERE Comp = '" . $name_in . "' AND Pass = '" . $pass_in . "';";
+        //echo $query;
+        //$query = "SELECT * FROM graders;";
         $result = mysqli_query($conn, $query);
 
         if (mysqli_num_rows($result) > 0) {
@@ -52,16 +53,14 @@
     // output data of each row
 
             while($row = mysqli_fetch_assoc($result)) {
-                if ($row['Pass'] === $pass_in) {
+                    echo $row;
                     $_SESSION['name'] = $row['Name'];
                     $_SESSION['comp'] = $name_in;
                     Header("Location: index.html");
-                }
-                else {
+            }
+        } else {
                 echo "try again.";
                 }
-            }
-        }
 
 
     $conn->close();
